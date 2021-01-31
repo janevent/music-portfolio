a69842cbde8408e194aa7c5c944fb1adb585439e
janeventer <janeanneventer@gmail.com>
2021-01-30 20:35:37 -0800

    create NavigationBar component

---
+++ ibra-portfolio/src/components/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavigationBar extends React.Component {
+    render(){
+        return (
+            <div className="NavigationBar"></div>
+        )
+    }
+}
