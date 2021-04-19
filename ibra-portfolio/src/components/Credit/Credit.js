d37d34cc94ea57860642c54b53984a9eedb4745d
janeventer <janeanneventer@gmail.com>
2021-04-19 07:08:48 -0700

    change Credit to use the useState hook

--- ibra-portfolio/src/components/Credit/Credit.js
+++ ibra-portfolio/src/components/Credit/Credit.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Credit.css';
 
 export default function Credit(){
+    const [name, setName] = useState('Nashad Abdu')
     return(
-        <div className="Credit">Background Photo by Nashad Abdu on Unsplash
+        <div className="Credit">Background Photo by {name} on Unsplash
         </div>
     )
 }
