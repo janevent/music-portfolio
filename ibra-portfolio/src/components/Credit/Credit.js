9a4f749d48f8da8148807b75a6744cf981ae37dc
janeventer <janeanneventer@gmail.com>
2021-04-19 06:43:16 -0700

    add styling to Credit Component

--- ibra-portfolio/src/components/Credit/Credit.js
+++ ibra-portfolio/src/components/Credit/Credit.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Credit.css';
 
 export default function Credit(){
-
     return(
-        <div className="Credit">Photo by <a href="https://unsplash.com/@nashadabdu?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nashad Abdu</a> on <a href="https://unsplash.com/@janeanne/likes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        <div className="Credit">Background Photo by Nashad Abdu on Unsplash
         </div>
     )
 }
