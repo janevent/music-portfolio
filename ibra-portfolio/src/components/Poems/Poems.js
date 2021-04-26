b6c2fdd7be74f7da09bc311056bff540eba6c7b7
janeventer <janeanneventer@gmail.com>
2021-04-26 08:29:53 -0700

    write function to display poem

--- ibra-portfolio/src/components/Poems/Poems.js
+++ ibra-portfolio/src/components/Poems/Poems.js
@@ -6,5 +6,17 @@ export default function Poems(props){
     const [title2, setTitle2] = useState("Life");
     const [poem1, setPoem1] = useState(props.poem1);
     const [poem2, setPoem2] = useState(props.poem2);
+
+    const returnPoem = (poem)=> {
+        let displayedPoem = poem.map(line => {
+            return <p>{line}</p>
+        })
+        return <div>{displayedPoem}</div>
+    }
+    return (
+        <div className="Poems">
+            {}
+        </div>
+    )
 }
 
