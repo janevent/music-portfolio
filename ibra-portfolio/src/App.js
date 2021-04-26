0b6abc5b8b9519b60e48738f05526ccb642e65fa
janeventer <janeanneventer@gmail.com>
2021-04-26 08:19:12 -0700

    import Poems and poem texts and display Poems component with data

--- ibra-portfolio/src/App.js
+++ ibra-portfolio/src/App.js
@@ -4,16 +4,20 @@ import Profile from './components/Profile.js';
 import SongsContainer from './components/SongsContainer/SongsContainer.js';
 import Credit from './components/Credit/Credit.js';
 import NavigationBar from './components/NavigationBar/NavigationBar.js';
+import Poems from './components/Poems/Poems.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import autobio from'./texts/autobio.js';
-import poem1 from './texts/poems.js';
+import poem1 from './texts/poem1.js';
+import poem2 from './texts/poem2.js';
 
 class App extends React.Component {
   state = {
     profile : {
       name: "Afrik",
-      autobio: autobio
+      autobio: autobio,
+      poem1: poem1,
+      poem2: poem2
     }
   }
 
@@ -29,6 +33,7 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/" render={(props) => <Profile {...props} profile={this.state.profile}/>} />
               <Route exact path="/songs" component={SongsContainer} />
+              <Route exact path="poems" render={(props) => <Poems {...props} poem1={this.state.poem1} poem2={this.state.poem2}/>} />
             </Switch>
             <Credit/>
           </div>
