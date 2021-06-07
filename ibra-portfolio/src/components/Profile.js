cc7f69aac2a46823630fdb71517e24e2599fa05f
janeventer <janeanneventer@gmail.com>
2021-06-06 17:39:07 -0700

    use hooks to set state from props and change background to cover when on profile

--- ibra-portfolio/src/components/Profile.js
+++ ibra-portfolio/src/components/Profile.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/Profile.css';
 import Credit from './Credit/Credit.js';
 
 
-export default class Profile extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            name: props.profile.name,
-            autobio: props.profile.autobio
-        }
-    }
+const Profile = (props) => {
+    
 
-    render(){
+    const [profile, setProfile] = useState(props.profile);
+   // const [autobio, setAutobio] = useState(props.profile)
+
+    useEffect(() => {
+        console.log("I have been mounted");
+        document.getElementsByClassName("App")[0].style.backgroundSize = "cover";
+       
+        return function cleanUp(){
+            document.getElementsByClassName("App")[0].style.backgroundSize = "contain";    
+        }
+    });
+   
         return(
             <div className="Profile">
                 <div className="profile-container">
-                    <h1>About {this.state.name}</h1>
-                    <div className="paragraphs">{this.state.autobio}</div>
+                    <h1>About {profile.name}</h1>
+                    <div className="paragraphs">{profile.autobio}</div>
                 </div>
                 <Credit/>
             </div>
         )
-    }
 }
+
+export default Profile;
 
